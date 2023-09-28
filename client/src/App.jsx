209caffb266e0292ba96b9c66daa55f12d169bac
jcaitou <71485747+jcaitou@users.noreply.github.com>
2023-09-28 16:19:29 +0800

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,58 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  HomeLayout,Landing,Register,Login,DashboardLayout,Error,AddJob,Profile,Admin,Stats,AllJobs
+} from './pages'
+import { checkDefaultTheme } from './utils/checkDefaultTheme';
+
+checkDefaultTheme();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {  
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+          },
+          { path: 'stats', element: <Stats /> },
+          {
+            path: 'all-jobs',
+            element: <AllJobs />,
+          },
+
+          {
+            path: 'profile',
+            element: <Profile />,
+          },
+          {
+            path: 'admin',
+            element: <Admin />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
